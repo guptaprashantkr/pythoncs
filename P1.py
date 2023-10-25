53b068c890ac449a16c584d003807dfcbf6270d8
guptaprashantkr <144984006+guptaprashantkr@users.noreply.github.com>
2023-10-25 12:25:37 +0530

    Update P1.py

--- P1.py
+++ P1.py
@@ -1,7 +1,11 @@
+# Python program to convert the temperature in degree centigrade to Fahrenheit.
 
+# Input from the user
 celsius = float(input("Enter the temperature in degrees Celsius: "))
 
+# Convert Celsius to Fahrenheit
 fahrenheit = (celsius * 9/5) + 32
 
 # Display the result
-print("Temperature in Fahrenheit is:" , fahrenheit)
+print(f"{celsius} degrees Celsius is equal to {fahrenheit:.2f} degrees Fahrenheit.")
+
