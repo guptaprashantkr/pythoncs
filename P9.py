d56449dbc1c6a08ce5c1810db81db758be19a897
guptaprashantkr <144984006+guptaprashantkr@users.noreply.github.com>
2023-10-25 12:32:29 +0530

    Update P9.py

--- P9.py
+++ P9.py
@@ -1,3 +1,5 @@
+# Python program to display all the multiples of 3 within the range 10 to 50.
+
 # Loop through the range and display multiples of 3
 print("Multiples of 3 within the range 10 to 50:")
 for number in range(10, 50):
